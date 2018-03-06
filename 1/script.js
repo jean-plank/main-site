7ce0ed46085230172cecdedbdba6f1be1d238f97
Axel Nussbaumer <nussbaume@eisti.eu>
2018-03-06 11:46:46 +0100

    [EDIT] finished tools for creating the story

--- 1/script.js
+++ 1/script.js
@@ -1,35 +1,43 @@
 /* constants */
-const DOMAIN = "http://jp1.blbl.ch/"
+const CONSTS = {}
+const FUNCTIONS = {}
 
-const IMG_MENU = new Image("jpthestory.jpg")
-const IMG_MARKET1 = new Image("market1.jpg")
-const IMG_MARKET2 = new Image("market2.jpg")
-const IMG_ENTREPOT = new Image("entrepot.jpg")
-const IMG_CHAMBRE = new Image("chambre.jpg")
-const IMG_FIN = new Image("fin.jpg")
-const IMG_MF = new Image("mf.png")
-const IMG_GP2 = new Image("gp_posey.png")
-const IMG_GP1 = new Image("gp1.png")
-const IMG_GP3 = new Image("Gp_interogatoire.png")
-const IMG_HENTAI = new Image("mfhentai.png")
-const IMG_DEALER = new Image("dealer.png")
-const IMG_VORANGE = new Image("vendeuse_orange.png")
+CONSTS.DOMAIN = "http://jp1.blbl.ch/"
 
-const CHAR_J = { name: 'Jean Plank', color: "#FEFEFE" }
-const CHAR_MF = { name: 'Miss Fourtune', color: "#d671a9" }
-const CHAR_V = { name: 'La Vendeuse', color: "#d671a9" }
-const CHAR_D = { name: 'Dealer', color: "#d671a9" }
+CONSTS.IMG_MENU = new Image("jpthestory.jpg")
+CONSTS.IMG_MARKET1 = new Image("market1.jpg")
+CONSTS.IMG_MARKET2 = new Image("market2.jpg")
+CONSTS.IMG_ENTREPOT = new Image("entrepot.jpg")
+CONSTS.IMG_CHAMBRE = new Image("chambre.jpg")
+CONSTS.IMG_FIN = new Image("fin.jpg")
+CONSTS.IMG_MF = new Image("mf.png")
+CONSTS.IMG_GP2 = new Image("gp_posey.png")
+CONSTS.IMG_GP1 = new Image("gp1.png")
+CONSTS.IMG_GP3 = new Image("Gp_interogatoire.png")
+CONSTS.IMG_HENTAI = new Image("mfhentai.png")
+CONSTS.IMG_DEALER = new Image("dealer.png")
+CONSTS.IMG_VORANGE = new Image("vendeuse_orange.png")
 
-const DIV_TEXT = document.getElementById('text')
-const DIV_CHAR_NAME = document.getElementById('char_name')
-const DIV_SCENE = document.createElement('div')
-DIV_SCENE.id = 'scene'
-const DIV_CHAR_IMG = document.createElement('div')
-DIV_CHAR_IMG.id = 'char_img'
+CONSTS.SND_JEVEUX = new Sound("jeveux.ogg")
+CONSTS.SND_JOUI = new Sound("joui.ogg")
+CONSTS.SND_JUTEUX = new Sound("juteux.ogg")
+CONSTS.SND_MAIN_THEME = new Sound("main_theme.ogg")
+CONSTS.SND_ORANGE = new Sound("orange.ogg")
+CONSTS.SND_PAN = new Sound("pan.mp3")
+CONSTS.SND_PIRATE = new Sound("pirate.mp3")
+CONSTS.SND_PLUSDEPOUDRE = new Sound("plusdepoudre.ogg")
+CONSTS.SND_THEME_GP = new Sound("theme_gp.ogg")
+CONSTS.SND_TOUTLEMONDE = new Sound("toutlemonde.ogg")
+CONSTS.SND_UNCOUP = new Sound("uncoup.ogg")
 
-const DIV_STORY = document.getElementById('passage')
-prepend(DIV_CHAR_IMG, DIV_STORY)
-prepend(DIV_SCENE, DIV_STORY)
+CONSTS.CHAR_J = { name: 'Jean Plank', color: "#FEFEFE" }
+CONSTS.CHAR_MF = { name: 'Miss Fourtune', color: "#d671a9" }
+CONSTS.CHAR_V = { name: 'La Vendeuse', color: "#d671a9" }
+CONSTS.CHAR_D = { name: 'Dealer', color: "#d671a9" }
+
+
+const DIV_SCENE = $('<div id="scene">')
+const DIV_CHAR_IMG = $('<div id="char_img">')
 
 
 /* classes */
@@ -39,10 +47,9 @@ function Image(name) {
 }
 
 Image.prototype.load = function() {
-    if (this.elt === null) {
-        this.elt = document.createElement('img')
-        this.elt.src = DOMAIN+'images/'+this.name
-    }
+    if (this.elt === null)
+        this.elt = $('<img>')
+            .attr('src', CONSTS.DOMAIN+'images/'+this.name)
 }
 
 
@@ -52,61 +59,73 @@ function Sound(name) {
 }
 
 Sound.prototype.load = function() {
-    if (this.elt === null) {
-        // this.elt = document.createElement('img')
-        // this.elt.src = DOMAIN+'sound/'+image.name
-    }
+    if (this.elt === null)
+        this.elt = $('<audio>')
+            .attr({
+                'src': CONSTS.DOMAIN+'sounds/'+this.name,
+                'preload': 'auto'
+            })
 }
 
 
 /* functions */
-// empties dom element elt
-function empty(elt) {
-    while (elt.firstChild) elt.removeChild(elt.firstChild)
-}
-
-
-function prepend(child, parent) {
-    parent.insertBefore(child, parent.firstChild)
-}
-
-
 // img, valid image { name: ..., elt: ... }
 // hide current scene and show img as new one
-function show_scene(img) {
-    empty(DIV_SCENE)
+FUNCTIONS.show_scene = function(img) {
+    DIV_SCENE.empty()
     img.load()
     DIV_SCENE.append(img.elt)
 }
 
 
 // show image as character
-function show_char(img) {
-    // empty(DIV_CHAR_IMG)
+FUNCTIONS.show_char = function(img) {
+    // DIV_CHAR_IMG.empty()
     img.load()
     DIV_CHAR_IMG.append(img.elt)
 }
 
 
-function hide_char(img) {
+FUNCTIONS.hide_char = function(img) {
     DIV_CHAR_IMG.removeChild(img.elt)
 }
 
 
-function play_music(music) {
-    // TODO
+FUNCTIONS.play_music = function(sound) {
+    sound.load()
+    sound.elt.prop('loop', true)
+    sound.elt[0].play()
+}
+
+FUNCTIONS.stop_music = function(sound) {
+    sound.elt[0].pause()
+}
+
+
+FUNCTIONS.play_sound = function(sound) {
+    sound.load()
+    sound.elt.prop('loop', false)
+    sound.elt[0].play()
 }
 
 
 // removes current text and sets text as new one
-function text(text) {
-    DIV_TEXT.textContent = text;
+FUNCTIONS.show_text = function(text) {
+    CONSTS.DIV_TEXT.text(text)
 }
 
 
 // char, valid character { name: ..., color: ... }
-function say(char, txt) {
-    DIV_CHAR_NAME.textContent = char.name
-    DIV_CHAR_NAME.style.color = char.color
-    text(txt)
+FUNCTIONS.get_div_char_name = function(char) {
+    return '<div id="char_name" style="color:'+char.color+';">'
+        + char.name + '</div>'
 }
+
+
+/* script */
+window.story.state.c = CONSTS
+window.story.state.f = FUNCTIONS
+
+$('body')
+    .prepend(DIV_CHAR_IMG)
+    .prepend(DIV_SCENE)

e42b680ff8d67ad60a32ccb82e8903ef4c2dd78a
Axel Nussbaumer <axel.nussbaumer@eisti.fr>
2018-08-29 15:41:30 +0200

    [DELETE] cleanup useless files

--- mocks.js
+++
@@ -1,8 +0,0 @@
-/* mocks */
-_ = {
-    extend: function (a, b) {},
-    template: function (source) { return function (data) { return source } },
-}
-
-Passage = { prototype: function () {} }
-Story = { prototype: function () {} }

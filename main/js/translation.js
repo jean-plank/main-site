c0262f7a176799a2374708bd7c1d39f8f2598ed7
Axel Nussbaumer <axel.nussbaumer@eisti.fr>
2018-09-18 13:27:05 +0200

    [EDIT] correct links translation; translation buttons no more <button>

--- main/js/translation.js
+++ main/js/translation.js
@@ -1,5 +1,6 @@
 const languages = ["fr", "en"];
 
+
 const translations = {
     "pretitle": {
         fr: "La véritable histoire de",
@@ -41,16 +42,16 @@ const translations = {
     },
     "jp2-title": {
         fr: "Naissance des Flammes de la Vengeance",
-        en: "Birth of the Flames of Vengeance",
+        en: "Birth of the Flames of Revenge",
     },
     // Jean Plank III - Le Vrai Visage de la Vengeance
     "jp3-summary": {
-        fr: "<i>Suite de la première fin de Jean Plank II</i><br><br>Jean-Plank, déformé par Saint Gède, part en quête de son visage.",
-        en: "<i>Sequel to the first ending of Jean Plank II</i><br><br>Jean-Plank, deformed by Saint Gède, seeks his lost face.",
+        fr: "<i>Suite de la première fin de Jean Plank II</i><br><br>Jean Plank, déformé par Saint Gède, part en quête de son visage.",
+        en: "<i>Sequel to the first ending of Jean Plank II</i><br><br>Jean Plank, deformed by Saint Gède, seeks his lost face.",
     },
     "jp3-title": {
         fr: "Le Vrai Visage de la Vengeance",
-        en: "The True Face of Vengeance",
+        en: "The True Face of Revenge",
     },
     // Jean Plank III - Valhalla Ouakbar
     "jp3b-summary": {
@@ -72,13 +73,76 @@ const translations = {
     },
 };
 
+
+const links = {
+    // Jean Plank I - The Story
+    "jp1-launch": {
+        fr: "fr/thestory",
+        en: "en/thestory",
+    },
+    "jp1-dl": {
+        fr: "https://dl.blbl.ch/jean-plank/fr/Jean Plank I - The Story.zip",
+        en: "https://dl.blbl.ch/jean-plank/en/Jean Plank I - The Story.zip",
+    },
+    // Jean Plank II - Naissance des Flammes de la Vengeance
+    "jp2-launch": {
+        fr: "fr/naissancedesflammesdelavengeance",
+        en: "en/birthoftheflamesofrevenge",
+    },
+    "jp2-dl": {
+        fr: "https://dl.blbl.ch/jean-plank/fr/Jean Plank II - Naissance des Flammes de la Vengeance.zip",
+        en: "https://dl.blbl.ch/jean-plank/en/Jean Plank II - Birth of the Flames of Revenge.zip",
+    },
+    // Jean Plank III - Le Vrai Visage de la Vengeance
+    "jp3-launch": {
+        fr: "fr/levraivisagedelavengeance",
+    },
+    "jp3-dl": {
+        fr: "https://dl.blbl.ch/jean-plank/fr/Jean Plank III - Les Flammes de la Vengeance.zip",
+    },
+    // Jean Plank III - Valhalla Ouakbar
+    "jp3b-launch": {
+    },
+    "jp3b-dl": {
+    },
+    // Jean Plank IV - Tout le Monde doit Payer
+    "jp4-launch": {
+    },
+    "jp4-dl": {
+    },
+};
+
 const $btns = $("#lang-btns");
+
+
+function changeLanguage(lang) {
+    $btns.children().removeClass("active");
+    $("#btn-"+lang).addClass("active");
+
+    $("[text]").each((_, elt) => {
+        const $elt = $(elt);
+        const text = translations[$elt.attr("text")][lang];
+        if (text !== undefined) $elt.html(text);
+    });
+
+    $("[link]").each((_, elt) => {
+        const $elt = $(elt);
+        const link = links[$elt.attr("link")][lang];
+        if (link === undefined) {
+            $elt.hide();
+        } else {
+            $elt.show();
+            $elt.attr("href", link);
+        }
+    });
+}
 
 
 function init() {
     for (let i = 0; i < languages.length; i++) {
-        $btns.append($("<button>")
+        $btns.append($("<div>")
             .attr("id", "btn-"+languages[i])
+            .addClass("button")
             .text(languages[i])
             .click(languages[i], (e) => changeLanguage(e.data)));
     }
@@ -90,16 +154,6 @@ function init() {
         changeLanguage("en");
     }
 
-}
-
-
-function changeLanguage(lang) {
-    $("[text]").each((_, elt) => {
-        const $elt = $(elt);
-        $elt.html(translations[$elt.attr("text")][lang]);
-        $btns.children().removeClass("active");
-        $("#btn-"+lang).addClass("active");
-    });
 }
 
 

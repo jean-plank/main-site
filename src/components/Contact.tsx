687f821afd7d1dbcc96938b80dc977bd7ecec1fe
Gurimarukin <naxel@laposte.net>
2020-02-09 13:30:33 +0100

    Add submitForm

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,7 +4,16 @@ import * as A from 'fp-ts/lib/Array'
 import * as NA from 'fp-ts/lib/NonEmptyArray'
 import * as O from 'fp-ts/lib/Option'
 import { pipe } from 'fp-ts/lib/pipeable'
-import { ChangeEvent, FunctionComponent, ReactNode, useState } from 'react'
+import {
+    ChangeEvent,
+    createRef,
+    forwardRef,
+    FunctionComponent,
+    ReactNode,
+    RefObject,
+    useImperativeHandle,
+    useState
+} from 'react'
 
 interface Choice {
     label: string
@@ -32,45 +41,70 @@ const option = (label: string, leadsTo?: Choice): ChoiceOption => {
     return { value, label, leadsTo: O.fromNullable(leadsTo) }
 }
 
-export const Contact: FunctionComponent = () => (
-    <div css={styles.container}>
-        <Select
-            choice={choice('Premier choix :', [
-                option('option 1'),
-                option(
-                    'option 2',
-                    choice('Deuxième choix :', [
-                        option('option 2 - 1'),
-                        option(
-                            'option 2 - 2',
-                            choice('Troisième choix :', [
-                                option('option 2 - 2 - 1'),
-                                option('option 2 - 2 - 2')
-                            ])
-                        )
-                    ])
-                )
-            ])}
-        />
-    </div>
-)
+export const Contact: FunctionComponent = () => {
+    const optionsGetable: RefObject<OptionsGetable> = createRef()
+
+    return (
+        <div css={styles.container}>
+            <Select
+                ref={optionsGetable}
+                i={0}
+                choice={choice('Premier choix :', [
+                    option('option 1'),
+                    option(
+                        'option 2',
+                        choice('Deuxième choix :', [
+                            option('option 2 - 1'),
+                            option(
+                                'option 2 - 2',
+                                choice('Troisième choix :', [
+                                    option('option 2 - 2 - 1'),
+                                    option('option 2 - 2 - 2')
+                                ])
+                            )
+                        ])
+                    )
+                ])}
+            />
+
+            <button onClick={submitForm}>Envoyer</button>
+        </div>
+    )
+
+    function submitForm() {
+        const res = pipe(
+            O.fromNullable(optionsGetable.current),
+            O.fold(
+                () => [],
+                _ => _.getOptions()
+            )
+        ).map(_ =>
+            pipe(
+                _,
+                O.map(opt => [opt.value, opt.label])
+            )
+        )
+        console.table(res)
+    }
+}
 
 const NONE = 'none'
 
+interface OptionsGetable {
+    getOptions: () => O.Option<ChoiceOption>[]
+}
+
 interface Props {
+    i: number
     choice: Choice
 }
 
-const Select: FunctionComponent<Props> = ({ choice }) => {
-    const [selected, setSelected] = useState<O.Option<ChoiceOption>>(O.none)
+const Select = forwardRef<OptionsGetable, Props>(({ i, choice }, ref) => {
+    useImperativeHandle(ref, () => ({ getOptions }))
 
-    pipe(
-        selected,
-        O.fold(
-            () => console.log(choice.label, 'none'),
-            _ => console.log(choice.label, 'some(', _.label, ')')
-        )
-    )
+    const optionsGetable: RefObject<OptionsGetable> = createRef()
+
+    const [selected, setSelected] = useState<O.Option<ChoiceOption>>(O.none)
 
     const value = pipe(
         selected,
@@ -84,13 +118,13 @@ const Select: FunctionComponent<Props> = ({ choice }) => {
         O.chain(_ => _.leadsTo),
         O.fold(
             () => null,
-            _ => <Select choice={_} />
+            _ => <Select ref={optionsGetable} i={i + 1} choice={_} />
         )
     )
     return (
         <label>
             {choice.label}
-            <select value={value} onChange={select}>
+            <select value={value} onChange={onChange}>
                 <option value={NONE} />
                 {choice.options.map(choice => (
                     <option key={choice.value} value={choice.value}>
@@ -102,15 +136,23 @@ const Select: FunctionComponent<Props> = ({ choice }) => {
         </label>
     )
 
-    function select(e: ChangeEvent<HTMLSelectElement>) {
-        setSelected(
-            pipe(
-                choice.options,
-                A.findFirst(_ => _.value === e.target.value)
-            )
+    function onChange(e: ChangeEvent<HTMLSelectElement>) {
+        const newChoice = pipe(
+            choice.options,
+            A.findFirst(_ => _.value === e.target.value)
         )
+        setSelected(newChoice)
     }
-}
+
+    function getOptions(): O.Option<ChoiceOption>[] {
+        const tail = pipe(
+            O.fromNullable(optionsGetable.current),
+            O.map(_ => _.getOptions()),
+            O.getOrElse(() => [])
+        )
+        return [selected, ...tail]
+    }
+})
 
 const styles = {
     container: css({

c387c63cbca0dad3bdb3d1d3a7deb4222dae582c
Gurimarukin <naxel@laposte.net>
2020-04-08 23:46:42 +0200

    Don't show wip Contact

--- src/components/Maintenance.tsx
+++ src/components/Maintenance.tsx
@@ -5,26 +5,24 @@ import { FunctionComponent } from 'react'
 import jpgs from '../../img/*.jpg'
 
 export const Maintenance: FunctionComponent = () => (
-    <div css={styles.container}>
-        <img src={jpgs.maintenance} css={styles.image} />
-    </div>
+  <div css={styles.container}>
+    <img src={jpgs.maintenance} css={styles.image} />
+  </div>
 )
 
 const styles = {
-    container: css({
-        height: '100%',
-        width: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        background: `url('${jpgs.bg_not_found}')`,
-        backgroundSize: 'cover',
-        boxShadow: '0 0 10px black'
-    }),
+  container: css({
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    background: `url('${jpgs.bg_not_found}')`,
+    backgroundSize: 'cover',
+    boxShadow: '0 0 10px black'
+  }),
 
-    image: css({
-        width: '50%',
-        maxHeight: '50vh',
-        objectFit: 'cover'
-    })
+  image: css({
+    maxHeight: '50vh'
+  })
 }

120531d725abc56f73d11d1e96a3fa369ecc1c08
Gurimarukin <naxel@laposte.net>
2020-04-19 11:13:46 +0200

    Removed hello world routes

--- packages/server/src/Routes.ts
+++ packages/server/src/Routes.ts
@@ -1,42 +1,6 @@
-import * as H from 'hyper-ts'
-import * as t from 'io-ts'
-import express from 'express'
-import { fromRequestHandler } from 'hyper-ts/lib/express'
-import { failure } from 'io-ts/lib/PathReporter'
-
-import { pipe } from 'main-site-shared/lib/fp'
-
 import { ContactController } from './controllers/ContactController'
 import { Route } from './models/Route'
 
 export const Routes = (contactController: ContactController): Route[] => [
-  ['get', '/hello/', hello],
-  ['get', '/hello/:name', helloName],
   ['post', '/contact', contactController.submitForm]
 ]
-
-const hello: H.Middleware<H.StatusOpen, H.ResponseEnded, never, void> = pipe(
-  H.status(H.Status.OK), // writes the response status
-  H.ichain(() => H.closeHeaders()), // tells hyper-ts that we're done with the headers
-  H.ichain(() => H.send('Hello.')) // sends the response as text
-)
-
-const helloName = pipe(
-  fromRequestHandler(express.text(), _ => undefined),
-  H.ichain(_ => H.decodeParam('name', t.string.decode)),
-  H.mapLeft(e => `invalid param: ${failure(e).join('\n')}`),
-  H.ichain(name =>
-    pipe(
-      H.status<string>(H.Status.OK),
-      H.ichain(_ => H.closeHeaders()),
-      H.ichain(_ => H.send(`Hello, ${name}.`))
-    )
-  ),
-  H.orElse(msg =>
-    pipe(
-      H.status(H.Status.BadRequest),
-      H.ichain(_ => H.closeHeaders()),
-      H.ichain(_ => H.send(msg))
-    )
-  )
-)
